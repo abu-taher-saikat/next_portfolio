39a295b2e6059b437f741126d6d3b1f92d0bd331
saikat1095 <mdabutahersaikat@gmail.com>
2021-06-23 02:36:28 +0600

    nav bar and heading done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Header from '../components/Header'
 import '../styles/globals.scss'
+import 'antd/dist/antd.css';
+
 
 function MyApp({ Component, pageProps }) {
   return(
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
-import headerImage from '../public/vercel.svg';
+import styles from '../styles/Hero.module.scss'
+import { Space } from 'antd';
+// import headerImage from '../public/vercel.svg';
+import HeroImage from '../public/codebysaikat.png';
+
 
 export default function Home() {
   return (
-  <div className={styles.homepage}>
-      <div className={styles.hero}>
-      <div className={styles.img}></div>
-      <div className={styles.content}>
-          <h4>Hi, I am Saikat,a Mern Stack Full Stack Developer</h4>
-          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-          <button className={styles.button}>Download Resume</button>
-      </div>
-    </div>
-  </div>
+  <Space size="middle" className={styles.homepage}>
+      <Space size="large" className={styles.hero}>
+        <div className={styles.img}>
+          <Image src={HeroImage} alt="code by saikat hero image"></Image>
+        </div>
+        <Space className={styles.content}>
+            <h4>Hi, I am Saikat,a Mern Stack Full Stack Developer</h4>
+            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+            <button className={styles.button}>Download Resume</button>
+        </Space>
+    </Space>
+  </Space>
   )
 }
