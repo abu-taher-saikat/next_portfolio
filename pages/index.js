148202da32451c209a78623e36443159412f549d
saikat1095 <mdabutahersaikat@gmail.com>
2021-06-23 12:04:31 +0600

    hero section done

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import { Space } from 'antd';
-import Link from 'next/link';
+import Link from 'next/Link';
 import styles from '../styles/Hero.module.scss'
 import HeroImage from '../public/codebysaikat.png';
 import {FaFacebookSquare} from 'react-icons/fa'
-// import {SiFiverr} from 'react-icons/si'
-// import {AiFillLinkedin} from 'react-icons/ai'
-// import {FiInstagram} from 'react-icons/fi'
+import {SiFiverr} from 'react-icons/si'
+import {AiFillLinkedin} from 'react-icons/ai'
+import {FiInstagram} from 'react-icons/fi'
 
 export default function Home() {
   return (
@@ -17,18 +17,20 @@ export default function Home() {
         <div className={styles.img}>
           <Image src={HeroImage} alt="code by saikat hero image"></Image>
         </div>
-        <Space className={styles.content}>
+        <div className={styles.content}>
             <h4>Hi, I am Saikat,a Mern Stack Full Stack Developer</h4>
-            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-            <button className={styles.button}>Download Resume</button>
-        </Space>
+            <p>I create attractive websites. That represents your identity. Exhibit your creativity. And that makes a brand of you.</p>
+            <p>I am a skilled full-stack web developer. Always I prefer hand-coded organized programming. I am an expert in html5, css3, bootstrap, JavaScript, react js, node js, express js and MongoDB.</p>
+            <Space size={'large'}>
+              <button className={styles.button}>Download Resume</button>
+            </Space>
+        </div>
         <Space className={styles.social}>
           <ul>
             <li><Link href="/facebook"><a><FaFacebookSquare /></a></Link></li>
-            <li><Link href="/facebooks"><a><FaFacebookSquare /></a></Link></li>
-            {/* <li><Link href="/fiverr"><a><SiFiverr /></a></Link></li>
+            <li><Link href="/fiverr"><a><SiFiverr /></a></Link></li>
             <li><Link href="/linkedin"><a><AiFillLinkedin /></a></Link></li>
-            <li><Link href="/instagram"><a><FiInstagram /></a></Link></li> */}
+            <li><Link href="/instagram"><a><FiInstagram /></a></Link></li>
           </ul>
         </Space>
     </Space>
